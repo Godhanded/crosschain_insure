51d14fd56eb9e83e6252b2505ea10d34b57abcdc
Godand <aezeigwe2003@gmail.com>
2022-10-26 11:40:14 +0000

    import axelar and set up constructor

--- contracts/insure.sol
+++ contracts/insure.sol
@@ -1,14 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
+
+import {IAxelarGateway} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol";
+import {IAxelarGasService} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
+import {AxelarExecutable} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/executables/AxelarExecutable.sol";
+import {StringToAddress, AddressToString} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/StringAddressUtils.sol";
+
 error DefiInsure__InvalidValue();
 error DefiInsure__NotOwner();
 error DefiInsure__TxFailed();
 
-contract DefiInsure {
+contract DefiInsure is AxelarExecutable{
+    using StringToAddress for string;
+    using AddressToString for address;
     struct entity {
         address entityAddr;
         uint256 deadline;
     }
+
+    IAxelarGasService public immutable i_gasReceiver;
 
     mapping(string => entity) private s_insured;
 
@@ -21,8 +31,13 @@ contract DefiInsure {
     uint256 constant MINIMUM_VALUE = 200;
     uint256 constant DECIMALS = 1e18;
 
-    constructor() {
+    constructor(
+        address gateway_,
+        address gasReceiver_
+
+    ) AxelarExecutable(gateway_) {
         s_owner = msg.sender;
+        i_gasReceiver= IAxelarGasService(gasReceiver_);
     }
 
     function payInsurance(string calldata id) external payable {
